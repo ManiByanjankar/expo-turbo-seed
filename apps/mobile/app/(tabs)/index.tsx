50b1c8f94a919724fea3e1619209a99b574796e5
ManiByanjankar <mani.byanjankar@rumsan.com>
2025-02-26 11:57:25 +0545

    screens ui only

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -9,12 +9,6 @@ export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
     >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcomes!</ThemedText>
